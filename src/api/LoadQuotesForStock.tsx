7545f9583f3a478cd7b5d3c98aa3bbc25bf00bfb
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-28 22:47:47 -0500

    Eliminated two anonymous functions for debug purposes in LoadQuotesForStock.tsx

--- src/api/LoadQuotesForStock.tsx
+++ src/api/LoadQuotesForStock.tsx
@@ -2,17 +2,25 @@ import axios from "axios";
 
 export const LoadQuotesForStock = (symbol = "", version = "sandbox", token = "") => {
 
-    const api = axios.create({
+    const api = axios.create ({
         baseURL: "https://cloud.iexapis.com/stable/"
     });
 
+    const apiGet = (res: any) => {
+        if (res.hasOwnProperty ("data")) {
+            return res.data;
+        } else {
+            throw Error (`API Error: GET request returned object but did not include desired data.`);
+        }
+    };
+
+    const errCat = (err: Error) => {
+        console.log (`API Error <LoadQuotesForStock>: ${err.message}`);
+    };
+
     return api.get (`stock/${symbol}/quote?token=${token}`)
-              .then ((res: any) => {
-                  return res.data;
-              })
-              .catch ((err: Error) => {
-                  console.log (`API Error <LoadQuotesForStock>: ${err.message}`);
-              });
+              .then (apiGet)
+              .catch (errCat);
 };
 
 export default LoadQuotesForStock;
