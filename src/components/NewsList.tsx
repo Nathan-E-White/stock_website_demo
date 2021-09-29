a15b8821d6ad67f976f867df89162aeb02a2079b
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-29 00:28:54 -0500

    Remodeled NewsList functional component to avoid the use of an anonymous function.

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -2,15 +2,19 @@ import * as React from "react";
 
 import NewsItem from "./NewsItem";
 
-export const NewsList = ({news}: any) => (
-    <div>{news.map ((newsItem: any, index: any) => {
-        return <div key={`news${index}`}>
-            <hr/>
-            <NewsItem {...newsItem}/>
-        </div>;
-    })}
-    </div>
-);
+export const NewsList = ({news}: any) => {
+
+    const newsMap = (newsItem: any, index: any) => {
+        return (
+            <div key={`news${index}`}>
+                <hr/>
+                <NewsItem {...newsItem}/>
+            </div>
+        );
+    };
+
+    return <div>{news.map (newsMap)}</div>;
+};
 
 // noinspection JSUnusedGlobalSymbols
 export default NewsList;
