83e3000662e9eb526594a35e695cedaca1f9aa7a
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-28 23:10:13 -0500

    Removed two conditional expressions and remodeled function component so it only had one return branch and one error branch.

--- src/components/ChartItem.tsx
+++ src/components/ChartItem.tsx
@@ -17,18 +17,38 @@ interface IChartItem {
 }
 
 export const ChartItem = (ci: IChartItem) => {
-    // noinspection MagicNumberJS
-    return (
-        <tr>
-            <th scope="row">{ci.date}</th>
-            <td>{ci.open}</td>
-            <td>{ci.high}</td>
-            <td>{ci.low}</td>
-            <td className={ci.stockIsUp ? 'text-success' : 'text-danger'}>
-                {ci.stockIsUp ? `${String.fromCharCode (0x25b2)} ${ci.close}` : `${String.fromCharCode ()} ${ci.close}`}
-            </td>
-        </tr>
-    );
+
+    let siuLabel: string, siuBody: string;
+
+    const hasOHL = ci.hasOwnProperty ("open") && ci.hasOwnProperty ("high") && ci.hasOwnProperty ("low");
+    const hasPro = ci.hasOwnProperty ("stockIsUp") && hasOHL;
+
+    if (hasPro) {
+        if (ci.stockIsUp) {
+            siuLabel = 'text-success';
+
+            /* Magic number isn't doing anything to code, just calling a character code */
+            // noinspection MagicNumberJS
+            siuBody = `${String.fromCharCode (0x25b2)} ${ci.close}`;
+        } else {
+            siuLabel = 'text-danger';
+            siuBody = `${String.fromCharCode ()} ${ci.close}`;
+
+        }
+
+        return (
+            <tr>
+                <th scope="row">{ci.date}</th>
+                <td>{ci.open}</td>
+                <td>{ci.high}</td>
+                <td>{ci.low}</td>
+                <td className={siuLabel}>{siuBody}</td>
+            </tr>
+        );
+    } else {
+        throw Error (`<ChartItem>: CI object given to function component was missing one or more fields.`);
+    }
 };
 
+// noinspection JSUnusedGlobalSymbols
 export default ChartItem;
