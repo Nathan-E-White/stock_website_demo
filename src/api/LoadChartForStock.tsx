4933f1a0eaa2397615c5c3298fde499e9bc7c4db
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-28 22:37:26 -0500

    Eliminated two anonymous functions for debug purposes in LoadChartForStock.tsx

--- src/api/LoadChartForStock.tsx
+++ src/api/LoadChartForStock.tsx
@@ -6,13 +6,21 @@ export const LoadChartForStock = (symbol: string, version: string, token: string
         baseURL: "https://cloud.iexapis.com/stable/"
     });
 
+    const apiRet = (res: any) => {
+        if (res.hasOwnProperty ("data")) {
+            return res.data;
+        } else {
+            throw Error (`API Error: GET call returned no data.`);
+        }
+    };
+
+    const errCat = (err: Error) => {
+        console.log (`API Error <LoadChartForStock>: ${err.message}`);
+    };
+
     return api.get (`stock/${symbol}/chart?token=${token}`)
-              .then ((res: any) => {
-                  return res.data;
-              })
-              .catch ((err: Error) => {
-                  console.log (`API Error <LoadChartForStock>: ${err.message}`);
-              });
+              .then (apiRet)
+              .catch (errCat);
 };
 
 export default LoadChartForStock;
