3fb991fc2b1407072a9981d9eee2cb460b66536d
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-28 21:26:03 -0500

    Created interface IChartItem and React functional class ChartItem in ChartItem.tsx.

---
+++ src/components/ChartItem.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+interface IChartItem {
+    date: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    stockIsUp: boolean;
+    // unadjustedVolume: number;
+    // change: number;
+    // changePercent: number;
+    // vwap: number;
+    // label: string;
+    // changeOverTime: string;
+}
+
+export const ChartItem = (ci: IChartItem) => {
+    return (
+        <tr>
+            <th scope="row">{ci.date}</th>
+            <td>{ci.open}</td>
+            <td>{ci.high}</td>
+            <td>{ci.low}</td>
+            <td className={ci.stockIsUp ? 'text-sucess' : 'text-danger'}>
+                {ci.stockIsUp ? String.fromCharCode (9650) + ' ' + ci.close : String.fromCharCode () + ' ' + ci.close}
+            </td>
+        </tr>
+    );
+};
+
+export default ChartItem;
