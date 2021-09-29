44a5b4b7b618db3f1f158fd5e62a1bb8a0e79bf7
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-28 23:47:35 -0500

    Copied over ChartTable.tsx from scratch work.

---
+++ src/components/ChartTable.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+const ChartItem = require ("./ChartItem");
+
+export const ChartTable = ({chart}: any) => (
+    <div>
+        <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Open</th>
+                <th scope="col">High</th>
+                <th scope="col">Low</th>
+                <th scope="col">Close</th>
+            </tr>
+            </thead>
+            <tbody>
+            {chart.map ((chartItem: any, index: any) => {
+                return chartItem.change < 0 ? (
+                    <ChartItem key={'chartTable' + index}{...chartItem} stockIsUp/>
+                ) : (
+                    <ChartItem key={'chartTable' + index}{...chartItem} />
+                );
+            })}
+            </tbody>
+        </table>
+    </div>
+);
+
+export default ChartTable;
