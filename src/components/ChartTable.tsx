5f00a62032830c846bc1f923f9a14d53160c7555
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-29 00:06:10 -0500

    Remodeled function to eliminate an anonymous function called by map for debug/optimization purposes later. Also changed require to import for ECMAScript 6 eventual migration.

--- src/components/ChartTable.tsx
+++ src/components/ChartTable.tsx
@@ -1,32 +1,39 @@
 import * as React from "react";
 
-const ChartItem = require ("./ChartItem");
+import ChartItem from "./ChartItem";
 
-export const ChartTable = ({chart}: any) => (
-    <div>
-        <table className="table">
-            <caption>Date and OHLC stock data</caption>
-            <thead>
-            <tr>
-                <th scope="col">Date</th>
-                <th scope="col">Open</th>
-                <th scope="col">High</th>
-                <th scope="col">Low</th>
-                <th scope="col">Close</th>
-            </tr>
-            </thead>
-            <tbody>
-            {chart.map ((chartItem: any, index: any) => {
-                return chartItem.change < 0 ? (
-                    <ChartItem key={'chartTable' + index}{...chartItem} stockIsUp/>
-                ) : (
-                    <ChartItem key={'chartTable' + index}{...chartItem} />
-                );
-            })}
-            </tbody>
-        </table>
-    </div>
-);
+export const ChartTable = ({chart}: any) => {
+
+    const chartMap = (chartItem: any, index: any) => {
+        let rtcElement;
+        if (chartItem.change < 0) {
+            rtcElement = <ChartItem key={`chartTable + ${index}`}{...chartItem} stockIsUp/>;
+        } else {
+            rtcElement = <ChartItem key={`chartTable + ${index}`}{...chartItem}/>;
+        }
+        return rtcElement;
+    };
+
+    return (
+        <div>
+            <table className="table">
+                <caption>Date and OHLC stock data</caption>
+                <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Open</th>
+                    <th scope="col">High</th>
+                    <th scope="col">Low</th>
+                    <th scope="col">Close</th>
+                </tr>
+                </thead>
+                <tbody>
+                {chart.map(chartMap)}
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
 // noinspection JSUnusedGlobalSymbols
 export default ChartTable;
