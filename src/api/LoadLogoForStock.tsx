b98074dcae8aec09f7055baf4776c9af803f0edf
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-28 22:44:31 -0500

    Eliminated two anonymous functions for debug purposes in LoadLogoForStock.tsx.

--- src/api/LoadLogoForStock.tsx
+++ src/api/LoadLogoForStock.tsx
@@ -6,13 +6,26 @@ export const LoadLogoForStock = (symbol: string, version: string, token: string)
         baseURL: "https://cloud.iexapis.com/stable/"
     });
 
+    const apiGet = (res: any) => {
+        if (res.hasOwnProperty ("data")) {
+            if (res.data.hasOwnProperty ("url")) {
+                return res.data.url;
+            } else {
+                throw Error (`API Error: GET Request wasn't empty but did not include desired payload.`);
+            }
+
+        } else {
+            throw Error (`API Error: GET Request returned no data.`);
+        }
+    };
+
+    const errCat = (err: Error) => {
+        console.log (`API Error <LoadLogoForStock>: ${err.message}`);
+    };
+
     return api.get (`stock/${symbol}/logo?token=${token}`)
-              .then ((res: any) => {
-                  return res.data.url;
-              })
-              .catch ((err: Error) => {
-                  console.log (`API Error <LoadLogoForStock>: ${err.message}`);
-              });
+              .then (apiGet)
+              .catch (errCat);
 };
 
 export default LoadLogoForStock;
