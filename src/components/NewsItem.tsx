b8148da9a105edb24c1d2151a677531534decdef
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-29 00:08:14 -0500

    Copied over NewsItem.tsx file data from the scratch workspace.

---
+++ src/components/NewsItem.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+interface INewsItem {
+    datetime: string;
+    headline: string;
+    source: string;
+    url: string;
+    summary: string;
+}
+
+export const NewsItem = (ni: INewsItem) => {
+    return (
+        <div>
+            <a href={ni.url} target="_blank" rel="noreferrer"><h3>{ni.headline}</h3></a>
+            <div>
+                Source: <em>{ni.source}</em>, {ni.datetime.substring (0, 10)}
+            </div>
+            <div>
+                <p>{ni.summary}</p>
+            </div>
+        </div>
+    );
+};
+
+export default NewsItem;
