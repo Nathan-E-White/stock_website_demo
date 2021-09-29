3f395d2f4c734bd2aa38cf132047cadf440fa1b8
Nathan-E-White <nwnn4@mail.missouri.edu>
2021-09-29 00:36:55 -0500

    Copied over the scratch work from SymbolForm.tsx, not sure if we will actually use it or not, we'll see.

---
+++ src/components/SymbolForm.tsx
@@ -0,0 +1,65 @@
+import * as React                                  from "react";
+import {ChangeEvent, Component}                    from "react";
+import {Button, Form, Container, Input, FormGroup} from "semantic-ui-react";
+
+
+interface ISymbolFormProps {
+    onSymbolChange: any;
+    onSymbolSubmit: any;
+    symbol: string;
+    submittedSymbol: string;
+}
+
+interface ISymbolFormState {
+}
+
+class SymbolForm extends Component<ISymbolFormProps, ISymbolFormState> {
+
+    constructor (props: ISymbolFormProps) {
+        super (props);
+        this.handleChange = this.handleChange.bind (this);
+        this.handleSubmit = this.handleSubmit.bind (this);
+    }
+
+    handleChange = (e: ChangeEvent, data: Object) => {
+        this.props.onSymbolChange(e, data);
+    };
+
+
+    handleSubmit = (e: any) => {
+        this.props.onSymbolSubmit (e.target.value);
+    };
+
+    render () {
+
+        // @ts-ignore
+        const symbol = this.props.symbol;
+        // @ts-ignore
+        const submittedSymbol = this.props.submittedSymbol;
+
+        return (
+            <div>
+                <Container>
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormGroup>
+                            <Input
+                                placeholder="Beep"
+                                onChange={this.handleChange}
+                                value={this.props.submittedSymbol}
+                            />
+                            <Button
+                                content="Submit"
+                            />
+                        </FormGroup>
+                    </Form>
+                </Container>
+                <strong>onChange:</strong>
+                <pre>{JSON.stringify ({symbol}, null, 2)}</pre>
+                <strong>OnSubmit:</strong>
+                <pre>{JSON.stringify ({submittedSymbol}, null, 2)}</pre>
+            </div>
+        );
+    }
+}
+
+export default SymbolForm;
